93c1ca36fc70d5e078c1bccbdc1fa2d052daf60b
areldai03 <dai.0127@icloud.com>
2024-03-19 11:24:41 +0900

    useEffectでbgcolor変更確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import {Main} from "../components/Main"
 
 export default function Home() {
+  useEffect(()=>{
+    document.body.style.backgroundColor = "lightblue";
+    return ()=>{
+        document.body.style.backgroundColor = "";
+    };
+});
   return (
     <Main page="index"/>
   );
